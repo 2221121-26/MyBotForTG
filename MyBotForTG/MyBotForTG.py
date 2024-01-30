377851f4c47d8287af68dbb99028a629f8b64cd4
Danil Yapparov <danil031203@gmail.com>
2024-01-30 19:24:24 +0300

    Эхо бот с message.send.coppy()

--- MyBotForTG/MyBotForTG.py
+++ MyBotForTG/MyBotForTG.py
@@ -1,1 +1,46 @@
+from email import message
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+BOT_TOKEN = '6510250499:AAF562b8O4vp4khaXOP39m4q1B2hMGEK0K4'
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение')
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения, кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+        print(message.model_dump_json(indent=4, exclude_none=True))    # печатать апдейты от сервера в красивом виде json'а
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
+        print(message.model_dump_json(indent=4, exclude_none=True))
+
+
+if __name__ == '__main__':
+    print("Okaaaaaaay, let's go")
+    dp.run_polling(bot)
